490d85992b21205a7f86fc87346fc95c839ab97a
Jeff Lasslett <jeff.lasslett@datataker.com.au>
2011-08-30 12:51:05 +1000

    Add xmobar and gridselect menu

--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+import XMonad.Hooks.SetWMName
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
@@ -6,13 +7,20 @@ import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Actions.GridSelect
 import System.IO
 
+
 main =
   do
+    xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
-      { manageHook = manageDocks <+> manageHook defaultConfig
-      , layoutHook = avoidStruts  $  layoutHook defaultConfig
-      , modMask = mod4Mask     -- Rebind Mod to the Windows key
-	  , terminal = "gnome-terminal"
-      }  `additionalKeys`
-      [ ((mod4Mask, xK_g), goToSelected defaultGSConfig)
-      ]
+        { startupHook = setWMName "LG3D"
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        } `additionalKeys`
+        [ ((mod4Mask, xK_g), goToSelected defaultGSConfig)
+        , ((mod4Mask, xK_s), spawnSelected defaultGSConfig ["gnome-terminal", "firefox", "gvim", "idea"])
+        ]
